5ea26ed5fbaad8cd6fbc74a2d6074f6060ce7a8f
Ajay Khatri <amajaying@gmail.com>
2022-07-26 23:59:05 +0530

    basic pattern question

---
+++ Basic Questions/dabbangPattern.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < a; j++)
+        {
+            cout << j + 1;
+           
+        }
+
+        for(int j=0; j<i;j++){
+            cout<<"**";
+        }
+
+        for (int j = a; j > 0; j--)
+        {
+            cout << j;
+        }
+        
+        cout << endl;
+        a--;
+    }
+    // for (int i = 0; i <= 8; i = i + 2)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+}
